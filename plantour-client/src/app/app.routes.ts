905483de78e0515192dfc85c1707035ba0cbe955
Serge Cherny <serguru@gmail.com>
2025-11-12 23:23:07 -0800

    Added login

--- plantour-client/src/app/app.routes.ts
+++ plantour-client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/users/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login }
+];
