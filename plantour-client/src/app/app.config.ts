9cf23c8584bd5aa8a785ef706030cbe616e1aa04
Serge Cherny <serguru@gmail.com>
2025-11-15 19:21:48 -0800

    Removed PrimeNG

--- plantour-client/src/app/app.config.ts
+++ plantour-client/src/app/app.config.ts
@@ -2,8 +2,6 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors  } from '@angular/common/http';
 import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { jwtInterceptor } from './interceptors/jwt.interceptor';
 
@@ -14,11 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideRouter(routes),
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    }),
     provideHttpClient(
       withInterceptors([jwtInterceptor])
     )
