a774fd8d113cab60b1094f98ae2f569e4da4e78b
Serge Cherny <serguru@gmail.com>
2025-11-16 18:54:17 -0800

    Added Messages Service

--- plantour-client/src/app/app.config.ts
+++ plantour-client/src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors  } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';import { jwtInterceptor } from './interceptors/jwt.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
+import { jwtInterceptor } from './interceptors/jwt.interceptor';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import Lara from '@primeuix/themes/lara';
 import Nora from '@primeuix/themes/nora';
 import Material from '@primeuix/themes/material';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,15 +17,16 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
     provideRouter(routes),
-    provideAnimationsAsync (),
+    provideAnimationsAsync(),
     provideHttpClient(
       withInterceptors([jwtInterceptor])
     ),
     providePrimeNG({
-            theme: {
-                preset: Lara
-            }
-        })
+      theme: {
+        preset: Lara
+      }
+    }),
+    MessageService
   ]
 };
 
--- plantour-client/src/app/app.ts
+++ plantour-client/src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UsersService } from './services/users-service';
+import { MessagesService } from './services/messages-service';
+import { ButtonModule } from 'primeng/button';
+import { ToastContainerComponent } from './components/shared/toast-container-component/toast-container-component';
+import { ModalDialogComponent } from './components/shared/modal-dialog-component/modal-dialog-component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, ToastContainerComponent, ModalDialogComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -12,9 +17,45 @@ export class App {
   protected readonly title = signal('plantour-client');
   protected readonly profile = signal<string>('');
   public usersService = inject(UsersService);
+  public mwessagesService = inject(MessagesService);
+
+  async delete() {
+    const result = await this.mwessagesService.openYesNoCancel({
+      title: 'Delete item',
+      message: 'Do you really want to delete this item?'
+    });
+
+    if (result === 'yes') {
+      // perform delete
+      this.mwessagesService.showInfo('Deleted', 'Item was deleted.');
+    } else if (result === 'no') {
+      this.mwessagesService.showInfo('Skipped', 'Item was not deleted.');
+    }
+    // result === 'cancel' -> user escaped/closed dialog
+  }
+
+  async delete1() {
+    const result = await this.mwessagesService.openOkCancel({
+      title: 'Delete item',
+      message: 'Do you really want to delete this item?'
+    });
+
+    if (result === 'ok') {
+      // perform delete
+      this.mwessagesService.showInfo('Deleted', 'Item was deleted.');
+    } else if (result === 'cancel') {
+      this.mwessagesService.showInfo('Skipped', 'Item was not deleted.');
+    }
+    // result === 'cancel' -> user escaped/closed dialog
+  }
+
+
+  show = () => {
+    this.delete1();
+  }
 
   getProfile = () => {
-      this.usersService.getProfile().subscribe(p => this.profile.set(JSON.stringify(p)))
+    this.usersService.getProfile().subscribe(p => this.profile.set(JSON.stringify(p)))
   }
 }
 
