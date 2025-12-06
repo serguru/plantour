1f2519c2779c14c34bdc6a8027e0191bea9d22e3
Serge Cherny <serguru@gmail.com>
2025-12-05 20:13:52 -0800

    Added lookups

---
+++ plantour-client/projects/shared-lib/src/services/lookup-service.ts
@@ -0,0 +1,147 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { ENVIRONMENT, EnvironmentConfig } from '../environment.token';
+
+export interface PackingStatusDto {
+  id: string;
+  name: string;
+  notes?: string | null;
+}
+
+export interface CommunicationTypeDto {
+  id: string;
+  name: string;
+  notes?: string | null;
+}
+
+export interface ThingCategoryDto {
+  id: string;
+  name: string;
+  notes?: string | null;
+}
+
+export interface TripStatusDto {
+  id: string;
+  name: string;
+}
+
+export interface ParticipantStatusDto {
+  id: string;
+  name: string;
+  notes?: string | null;
+}
+
+export interface UnitDto {
+  id: string;
+  name: string;
+}
+
+export interface LookupsResponse {
+  packingStatuses: PackingStatusDto[];
+  communicationTypes: CommunicationTypeDto[];
+  thingCategories: ThingCategoryDto[];
+  tripStatuses: TripStatusDto[];
+  participantStatuses: ParticipantStatusDto[];
+  units: UnitDto[];
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LookupService {
+  private apiUrl: string;
+  private lookups: LookupsResponse | null = null;
+
+  constructor(
+    private http: HttpClient,
+    @Inject(ENVIRONMENT) private environment: EnvironmentConfig
+  ) {
+    this.apiUrl = `${environment.apiUrl}/api/lookups`;
+  }
+
+  private loadLookupsIfNeeded(): Observable<LookupsResponse> {
+    if (this.lookups) {
+      return of(this.lookups);
+    }
+
+    return this.http.get<LookupsResponse>(this.apiUrl).pipe(
+      tap((response) => {
+        this.lookups = response;
+      })
+    );
+  }
+
+  getPackingStatuses(): Observable<PackingStatusDto[]> {
+    return new Observable((observer) => {
+      this.loadLookupsIfNeeded().subscribe({
+        next: (lookups) => {
+          observer.next(lookups.packingStatuses);
+          observer.complete();
+        },
+        error: (err) => observer.error(err),
+      });
+    });
+  }
+
+  getCommunicationTypes(): Observable<CommunicationTypeDto[]> {
+    return new Observable((observer) => {
+      this.loadLookupsIfNeeded().subscribe({
+        next: (lookups) => {
+          observer.next(lookups.communicationTypes);
+          observer.complete();
+        },
+        error: (err) => observer.error(err),
+      });
+    });
+  }
+
+  getThingCategories(): Observable<ThingCategoryDto[]> {
+    return new Observable((observer) => {
+      this.loadLookupsIfNeeded().subscribe({
+        next: (lookups) => {
+          observer.next(lookups.thingCategories);
+          observer.complete();
+        },
+        error: (err) => observer.error(err),
+      });
+    });
+  }
+
+  getTripStatuses(): Observable<TripStatusDto[]> {
+    return new Observable((observer) => {
+      this.loadLookupsIfNeeded().subscribe({
+        next: (lookups) => {
+          observer.next(lookups.tripStatuses);
+          observer.complete();
+        },
+        error: (err) => observer.error(err),
+      });
+    });
+  }
+
+  getParticipantStatuses(): Observable<ParticipantStatusDto[]> {
+    return new Observable((observer) => {
+      this.loadLookupsIfNeeded().subscribe({
+        next: (lookups) => {
+          observer.next(lookups.participantStatuses);
+          observer.complete();
+        },
+        error: (err) => observer.error(err),
+      });
+    });
+  }
+
+  getUnits(): Observable<UnitDto[]> {
+    return new Observable((observer) => {
+      this.loadLookupsIfNeeded().subscribe({
+        next: (lookups) => {
+          observer.next(lookups.units);
+          observer.complete();
+        },
+        error: (err) => observer.error(err),
+      });
+    });
+  }
+}
