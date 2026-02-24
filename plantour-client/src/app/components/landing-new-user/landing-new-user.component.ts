b24bf37201e929d065602cda92c2b0d07f29043a
Serge Cherny <serguru@gmail.com>
2026-02-23 23:13:36 -0800

    Created PalnsPanel

--- plantour-client/src/app/components/landing-new-user/landing-new-user.component.ts
+++ plantour-client/src/app/components/landing-new-user/landing-new-user.component.ts
@@ -1,34 +1,6 @@
-import { Component, inject, OnInit, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UsersService } from '../../services/users-service';
-import { PaddleService } from '../../services/paddle-service';
-import { AppButton } from '../button/button-component';
-import { LandingDto, LandingService, PlanDto } from '../../services/landing-service';
-import { ENVIRONMENT, EnvironmentConfig } from '../../../environment.token';
-
-interface PlanFeature {
-  label: string;
-  value: string | boolean;
-  highlight?: boolean;
-}
-
-interface Plan {
-  name: string;
-  monthlyPrice: string;
-  annualPrice?: string;
-  description: string;
-  isPopular?: boolean;
-  monthlyButtonText: string;
-  annualButtonText?: string;
-  monthlyPriceUrl?: string;
-  annualPriceUrl?: string;
-  features: PlanFeature[];
-  allowedItems: number;
-  allowedTravelers: number;
-  allowedAiPrompts: number;
-  extendedAiAllowed: boolean;
-
-}
+import { PlansPanelComponent } from '../plans-panel/plans-panel.component';
 
 interface LandingFeature {
   icon: string;
@@ -43,26 +15,10 @@ interface LandingFeature {
   templateUrl: './landing-new-user.component.html',
   styleUrl: './landing-new-user.component.scss'
 })
-export class LandingNewUserComponent implements OnInit {
+export class LandingNewUserComponent {
+  plansPanelComponent = PlansPanelComponent;
 
-  constructor(
-    @Inject(ENVIRONMENT) private environment: EnvironmentConfig
-  ) { }
-
-  paddleService = inject(PaddleService);
-  landingService = inject(LandingService);
-
-  slogan = 'Pack smart. Travel better.';
   subSlogan = 'Never forget an item again. Organize your packing with smart lists, categories, and seamless group coordination. Get AI-powered packing recommendations tailored to your destination.';
-
-  usersService = inject(UsersService);
-  plans: Plan[] = [];
-
-  ngOnInit(): void {
-    this.landingService.getLandingData().subscribe((data: LandingDto) => {
-      this.setPlans(data);
-    });
-  }
 
 
   featureList: LandingFeature[] = [
@@ -99,138 +55,4 @@ export class LandingNewUserComponent implements OnInit {
   ];
 
 
-  featuresByPlan = (plan: PlanDto) => {
-    return [
-      { label: 'Max Items', value: `${plan.allowedItems > 0 ? plan.allowedItems + ' per trip' : 'Unlimited'}` },
-      { label: 'Max Participants', value: `${plan.allowedTravelers > 0 ? plan.allowedTravelers + ' per trip' : 'Unlimited'}` },
-      { label: 'PDF Export', value: true },
-      { label: 'AI Suggestions', value: `${plan.allowedAiPrompts > 0 ? plan.allowedAiPrompts : 'Unlimited'} ${plan.extendedAiAllowed ? 'extended' : 'regular'} per day` },
-      { label: 'Shared Items', value: true },
-    ]
-  }
-
-  setPlans(data: LandingDto): void {
-    // this.plans = [
-    //   {
-    //     name: data.trialPlanName || "",
-    //     monthlyPrice: "0",
-    //     description: 'For small trips and light packers',
-    //     monthlyButtonText: 'Join Free',
-    //     features: [
-    //       { label: 'Duration', value: 'Unlimited', highlight: true },
-    //       { label: 'Max Items', value: '10 per trip' },
-    //       { label: 'Participants', value: 'Max 2' },
-    //       { label: 'PDF Export', value: true },
-    //       { label: 'AI Suggestions', value: '30 / month' },
-    //       { label: 'Shared Items', value: true },
-    //     ]
-    //   },
-    //   {
-    //     name: data.basePlanName || "",
-    //     monthlyPrice: data.basePlanMonthly || "",
-    //     annualPrice: data.basePlanYearly || "",
-    //     description: 'Perfect for families and small groups',
-    //     isPopular: true,
-    //     monthlyButtonText: 'Start monthly',
-    //     annualButtonText: 'Start yearly',
-    //     monthlyPriceLink: data.baseMonthlyPriceUrl || "",
-    //     annualPriceLink: data.baseYearlyPriceUrl || "",
-    //     features: [
-    //       { label: 'Duration', value: 'Unlimited' },
-    //       { label: 'Max Items', value: 'Unlimited', highlight: true },
-    //       { label: 'Participants', value: 'Max 5' },
-    //       { label: 'PDF Export', value: true },
-    //       { label: 'AI Suggestions', value: '10 / day' },
-    //       { label: 'Shared Items', value: true },
-    //     ]
-    //   },
-    //   {
-    //     name: data.proPlanName || "",
-    //     monthlyPrice: data.proPlanMonthly || "",
-    //     annualPrice: data.proPlanYearly || "",
-    //     description: 'Ideal for large groups and expeditions',
-    //     monthlyButtonText: 'Go monthly',
-    //     annualButtonText: 'Go yearly',
-    //     monthlyPriceLink: data.proMonthlyPriceUrl || "",
-    //     annualPriceLink: data.proYearlyPriceUrl || "",
-    //     features: [
-    //       { label: 'Duration', value: 'Unlimited' },
-    //       { label: 'Max Items', value: 'Unlimited' },
-    //       { label: 'Participants', value: 'Unlimited', highlight: true },
-    //       { label: 'PDF Export', value: true },
-    //       { label: 'AI Suggestions', value: '100 / day', highlight: true },
-    //       { label: 'Shared Items', value: true },
-    //     ]
-    //   }
-    // ];
-
-    const plans: any[] = [];
-
-    data.plans.forEach(plan => {
-
-
-
-      switch (plan.name) {
-        case 'Starter':
-          plans.push(
-            {
-              name: "Starter",
-              monthlyPrice: "0",
-              description: 'For small trips and light packers',
-              monthlyButtonText: 'Join Free',
-              monthlyPriceUrl: '/sign-up',
-              features: this.featuresByPlan(plan),
-              order: 1
-            }
-          );
-          break;
-        case 'Family':
-
-          const monthlyPriceObject = plan.prices?.find(p => p.name === 'PriceFamilyMonthly')!;
-          const yearlyPriceObject = plan.prices?.find(p => p.name === 'PriceFamilyYearly')!;
-
-          plans.push(
-            {
-              name: "Family",
-              monthlyPrice: (monthlyPriceObject?.valueCents / 100).toFixed(2),
-              annualPrice: (yearlyPriceObject?.valueCents / 100).toFixed(2),
-              description: plan.notes || "",
-              isPopular: true,
-              monthlyButtonText: 'Start monthly',
-              annualButtonText: 'Start yearly',
-              monthlyPriceUrl: `/checkout/${monthlyPriceObject.paddlePriceId}`,
-              annualPriceUrl: `/checkout/${yearlyPriceObject.paddlePriceId}`,
-              features: this.featuresByPlan(plan),
-              order: 2
-            }
-          );
-          break;
-        case 'Expedition':
-          const monthlyPriceObject1 = plan.prices?.find(p => p.name === 'PriceExpeditionMonthly')!;
-          const yearlyPriceObject1 = plan.prices?.find(p => p.name === 'PriceExpeditionYearly')!;
-
-
-          plans.push(
-            {
-              name: "Expedition",
-              monthlyPrice: (monthlyPriceObject1?.valueCents / 100).toFixed(2),
-              annualPrice: (yearlyPriceObject1?.valueCents / 100).toFixed(2),
-              description: plan.notes || "",
-              monthlyButtonText: 'Go monthly',
-              annualButtonText: 'Go yearly',
-              monthlyPriceUrl: `/checkout/${monthlyPriceObject1.paddlePriceId}`,
-              annualPriceUrl: `/checkout/${yearlyPriceObject1.paddlePriceId}`,
-              features: this.featuresByPlan(plan),
-              order: 3
-            }
-          );
-          break;
-        default:
-          throw new Error(`Unknown plan name: ${plan.name}`);
-      }
-    });
-
-    this.plans = plans.sort((a, b) => a.order - b.order);
-
-  }
 }
