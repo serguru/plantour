98ddac9de0a7b889158c77e3aa7ba2724904add2
Serge Cherny <serguru@gmail.com>
2026-03-23 14:19:59 -0700

    Version

--- plantour-client/src/environments/environment.pred-prod.ts
+++ plantour-client/src/environments/environment.pred-prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   environment: "pred-prod",
-  // Put pre-production API endpoints and feature flags here
   api: {
-    baseUrl: 'https://plantour-server-qa.onrender.com'
+    baseUrl: 'https://plantour-server-production.onrender.com'
   },
-  clientUrl: 'https://plantour-client-qa.onrender.com',
+  clientUrl: 'https://plantour-client-production.onrender.com',
   googleClientId: '256558134062-f78noulvdiq52n7bfmmh0cm23j91942s.apps.googleusercontent.com',
   facebookAppId: '2076632839798115',
   turnstileSiteKey: '',
-  version: '0.1.0'
+  version: '2.15.7'
 };
