5862c57cfe4987a6263055a522a13393cb40f31c
Serge Cherny <serguru@gmail.com>
2026-04-14 13:16:21 -0700

    Empty

--- plantour-server/DB/Scripts/1 plantour-tables.sql
+++ plantour-server/DB/Scripts/1 plantour-tables.sql
@@ -1350,3 +1350,4 @@ create index ix_logs_created_at on plantour.logs (created_at desc);
 insert into plantour.superusers(
 	email, name, phone, notes, hashed_password)
 	values ('admin@plantour.com', 'admin', null, null, '9803301a1675b7a1fca658178871ab46');
+    
